41ed001a69de052babcb9d1700ede2969893da69
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-10-06 16:54:41 +0600

    comment-out some unnecessary code

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,12 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 
-import Logo from "../assets/img/logo_light.png"
+import Logo from "../assets/img/logo_light.png"  // Before using the image, you have to import it.
 
 const Home = () => {
   return (
     <View style={styles.container}>
 
-        <Image source={Logo} />
+        <Image source={Logo} style={styles.img} />
 
         <Text style={styles.title}>The Number 1</Text>
 
@@ -14,9 +14,9 @@ const Home = () => {
             Reading List App
         </Text>
 
-        <View style={styles.card}>
+        {/* <View style={styles.card}>
             <Text>Hello, this is a Card</Text>
-        </View>
+        </View> */}
     </View>
   )
 }
@@ -34,16 +34,19 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 18,
   },
-  card: {
-    backgroundColor: '#fff',
-    padding: 20,
-    borderRadius: 5,
-    // iOS shadow properties
-    shadowColor: '#000',
-    shadowOffset: { width: 4, height: 4 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    // Android shadow property
-    elevation: 4,
-  }
+  img: {
+    marginVertical: 20, 
+  },
+//   card: {
+//     backgroundColor: '#fff',
+//     padding: 20,
+//     borderRadius: 5,
+//     // iOS shadow properties
+//     shadowColor: '#000',
+//     shadowOffset: { width: 4, height: 4 },
+//     shadowOpacity: 0.1,
+//     shadowRadius: 4,
+//     // Android shadow property
+//     elevation: 4,
+//   }
 })
