7ddfd79fda7e556f23d4ab5885054e2124507284
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-10-07 15:53:12 +0600

    add dark and light theme with dynamic statusBar

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, useColorScheme, View } from 'react-native'
 import { Slot, Stack } from 'expo-router'
 import { Colors } from '../constants/Colors';
+import { StatusBar } from 'expo-status-bar';
 
 const Rootlayout = () => {
 
@@ -10,34 +11,37 @@ const Rootlayout = () => {
   const theme = Colors[colorScheme] ?? Colors.light;  // The ?? will work as a fallback if colorScheme is null
 
   return (
-    <View style={{flex: 1}}>
-      <Stack screenOptions={{ 
-        headerStyle: { backgroundColor: theme.navBackground },
-        headerTitleStyle: { fontWeight: 'bold' },
-        headerTitleAlign: 'center',
-        headerTintColor: theme.title,
-        }}>
-        <Stack.Screen
-          name="index"
-          options={{
-            title: 'Home'
-          }}
-        />
-        <Stack.Screen
-          name="about"
-          options={{
-            title: 'About'
-          }}
-        />
-        <Stack.Screen
-          name="contact"
-          options={{
-            title: 'Contact'
-          }}
-        />
-      </Stack>
-      <Text>Footer</Text>
-    </View>
+    <>
+      <StatusBar value="auto" />
+      <View style={{flex: 1}}>
+        <Stack screenOptions={{ 
+          headerStyle: { backgroundColor: theme.navBackground },
+          headerTitleStyle: { fontWeight: 'bold' },
+          headerTitleAlign: 'center',
+          headerTintColor: theme.title,
+          }}>
+          <Stack.Screen
+            name="index"
+            options={{
+              title: 'Home'
+            }}
+          />
+          <Stack.Screen
+            name="about"
+            options={{
+              title: 'About'
+            }}
+          />
+          <Stack.Screen
+            name="contact"
+            options={{
+              title: 'Contact'
+            }}
+          />
+        </Stack>
+        <Text>Footer</Text>
+      </View>
+    </>
   )
 }
 
--- app/about.jsx
+++ app/about.jsx
@@ -1,9 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, useColorScheme } from 'react-native';
 import { Link } from 'expo-router';
+import { Colors } from '../constants/Colors';
 
 const About = () => {
+
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {backgroundColor: theme.background}]}>
       <Text style={styles.title}>About Page</Text>
       <Link style={styles.link} href="/">Go back to Home Page</Link>
     </View>
